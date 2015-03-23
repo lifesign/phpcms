5f402354df384de47bad08c5680597bf4693a211
lifesign <leocloudy@gmail.com>
2015-03-23 23:29:17 +0800

    add www directory

---
+++ common.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+
+//PHPCMS 核心目录
+define('PHPCMS_PATH', dirname(__FILE__) . DS);
+//定义web根目录
+define('WEB_ROOT_PATH', PHPCMS_PATH . 'www' . DS);
---
+++ www/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  index.php PHPCMS 入口
+ *
+ * @copyright			(C) 2005-2010 PHPCMS
+ * @license				http://www.phpcms.cn/license/
+ * @lastmodify			2015-3-23
+ */
+
+require '../common.php';
+
+include PHPCMS_PATH.'/phpcms/base.php';
+
+pc_base::creat_app();
+
+?>
