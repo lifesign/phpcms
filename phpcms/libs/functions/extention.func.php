5b73bfe5b7eb3ad778f2973de4e8df77fa0e5c62
lifesign <leocloudy@gmail.com>
2015-03-26 23:43:00 +0800

    add env function

--- phpcms/libs/functions/extention.func.php
+++ phpcms/libs/functions/extention.func.php
@@ -7,4 +7,37 @@
  * @lastmodify			2010-10-27
  */
  
-?>
+
+ /**
+ * 获取环境变量的值
+ * @param  [string] $key
+ * @param  [mixed]  $default
+ * @return [mixed]
+ */
+function env($key, $default = null)
+{
+    $value = getenv($key);
+
+    if ($value === false) return $default;
+
+    switch (strtolower($value))
+    {
+        case 'true':
+        case '(true)':
+            return true;
+
+        case 'false':
+        case '(false)':
+            return false;
+
+        case 'null':
+        case '(null)':
+            return null;
+
+        case 'empty':
+        case '(empty)':
+            return '';
+    }
+
+    return $value;
+}
