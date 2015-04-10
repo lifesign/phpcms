a9836bde3810cf94775e0c7fe88aeb3589ebaa8a
lifesign <leocloudy@gmail.com>
2015-04-10 22:29:05 +0800

    add postjs config

--- phpcms/modules/poster/functions/global.func.php
+++ phpcms/modules/poster/functions/global.func.php
@@ -3,15 +3,24 @@
  * 广告模板配置函数
  */
 function get_types() {
-	$poster_template = getcache('poster_template_'.get_siteid(), 'commons');
+
+	//如果没有读取到对应站点的广告模板缓存，则从配置文件取
+	//如需增加模板统一在配置文件中添加，子站点则可以通过模板配置以及templates下对应的风格覆盖主站点的模板。
+	$poster_cache_name = 'poster_template_'.get_siteid();
+
+	$poster_template = getcache($poster_cache_name, 'commons');
+
+	if (empty($poster_template)) {
+		$poster_template = pc_base::load_config('poster');
+		setcache($poster_cache_name, $poster_template, 'commons');
+	}
+
 	$TYPES = array();
 	if (is_array($poster_template) && !empty($poster_template)){
 		foreach ($poster_template as $k => $template) {
 			$TYPES[$k] = $template['name'];
 		}
-	} else {
-		$TYPES = array('banner'=>L('banner', '', 'poster'), 'fixure'=>L('fixure'), 'float'=>L('float'), 'couplet'=>L('couplet'), 'imagechange'=>L('imagechange'),  'imagelist'=>L('imagelist'), 'text'=>L('text'), 'code'=>L('code'),);
 	}
+
 	return $TYPES;
 }
-?>
